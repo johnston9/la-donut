79f445c50b8833f043427c3d92a2eaf56a8ac012
johnston9 <davejohnston9@gmail.com>
2021-06-05 22:30:28 +0000

    Add shop page and beginnning of the select menu

--- home/views.py
+++ home/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def shop(request):
+    """ A view to return the shop page """
+
+    return render(request, 'home/shop.html')
