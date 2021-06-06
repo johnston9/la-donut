d364aa836e51c4394667093b92c47ff5f66aaf63
johnston9 <davejohnston9@gmail.com>
2021-06-06 14:10:32 +0000

    add product and category models in Products/models

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -10,3 +14,21 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    flavour = models.CharField(max_length=254)
+    description = models.TextField()
+    shop_price = models.DecimalField(max_digits=6, decimal_places=2)
+    web_price = models.DecimalField(max_digits=6, decimal_places=2)
+    web_for6_price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    storage = models.CharField(max_length=254)
+    is_vegan = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
