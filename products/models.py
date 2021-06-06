7ba5a8079d0f0892c6cd759f846d2834535b0205
johnston9 <davejohnston9@gmail.com>
2021-06-06 17:37:03 +0000

    added some products and changes to the product model

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -19,15 +19,15 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
-    flavour = models.CharField(max_length=254)
+    flavour = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
-    shop_price = models.DecimalField(max_digits=6, decimal_places=2)
-    web_price = models.DecimalField(max_digits=6, decimal_places=2)
-    web_for6_price = models.DecimalField(max_digits=6, decimal_places=2)
+    shop_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    web_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    web_for_6_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    storage = models.CharField(max_length=254)
+    storage = models.CharField(max_length=254, null=True, blank=True)
     is_vegan = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
