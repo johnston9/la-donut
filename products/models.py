fdf7b78b9718cc0bc92acb355f09194307114988
johnston9 <davejohnston9@gmail.com>
2021-06-06 23:48:28 +0000

    resized product card margins and padding

--- products/models.py
+++ products/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     shop_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     web_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     web_for_6_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=1, decimal_places=0, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     storage = models.CharField(max_length=254, null=True, blank=True)
