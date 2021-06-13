2170c6239a0e0f97cd579cf4e35e9262d5ea05d9
johnston9 <davejohnston9@gmail.com>
2021-06-13 18:28:54 +0000

    added bag details to context...work in progress

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,17 +10,57 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        sizes = Size.objects.filter(product__name=product.name)
-        total += quantity * product.web_price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-            'sizes': sizes,
-        })
+    for item_id, item_data in bag.items():
+        if 'items_with_size' in item_data.keys():
+            product = get_object_or_404(Product, pk=item_id)
+            sizes = get_object_or_404(Size, name=product.name)
+            for size, quantity-price in item_data['items_with_size'].items():
+                size = size
+                quantity-price = quantity-price
+            for quantity, price in item_data['items_with_size'][size].items():
+                quantity = quantity
+                price = price
+                total += quantity * price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'size': size,
+                    'price': price,
+                    'quantity': quantity,
+                    'product': product,
+                    'sizes': sizes,
+            })
+        
+        elif 'items_with_forsix' in item_data.keys():
+            product = get_object_or_404(Product, pk=item_id)
+            forsixes = get_object_or_404(Forsix, name=product.name)
+            for size, quantity-price in item_data['items_with_size'].items():
+                size = size
+                quantity-price = quantity-price
+            for quantity, price in item_data['items_with_size'][size].items():
+                quantity = quantity
+                price = price
+                total += quantity * price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'size': size,
+                    'price': price,
+                    'quantity': quantity,
+                    'product': product,
+                    'forsixes': forsixes
+            })
+
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.web_price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+                'price': product.web_price
+            })
 
     if total != 0 and total < settings.FREE_DELIVERY_TOTAL:
         delivery = 10
