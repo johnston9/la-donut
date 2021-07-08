aa96c79481cb0782086a8e366ea5446cb799f7fe
johnston9 <davejohnston9@gmail.com>
2021-07-08 01:20:24 +0000

    Added pagination to comments to not refresh the page and have them in reverse order for chat to really work...major battle

--- recipe/urls.py
+++ recipe/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('get_recipe/<int:recipe_id>/', views.get_recipe, name='get_recipe'),
     path('latest_recipe/', views.latest_recipe, name='latest_recipe'),
     path('add_recipe/', views.add_recipe, name='add_recipe'),
-    path('delete_recipe/recipe_id/', views.delete_recipe, name='delete_recipe'),
+    path('delete_recipe/<int:recipe_id>/', views.delete_recipe, name='delete_recipe'),
     path('chat/', views.chat, name='chat'),
 ]
